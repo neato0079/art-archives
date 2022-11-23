dcd2e4023f40916d945249e3ffbf1807d6a7aff2
neato0079 <neato0079@gmail.com>
2022-11-22 20:46:11 -0800

    Impement download function. Downloads file but wrong format?

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const fs = require('fs');
+const axios = require('axios')
 
-const twitterPic = 'https://assets-global.website-files.com/5a016d51240da900013d2ea2/5fc8e1f4bc8a02aecf06f035_eyeem-23716958-121079333-(1)%20(1).png'
-const mediaStream = fs.createReadStream(twitterPic)
-const writeStream = fs.createWriteStream('./output');
+const downloadImage = async (url, path) => {
+    const response = await axios.get(url);
+    console.log(`Axios get status:${response.status}`)
+    // const blob = await response.blob();
+    // const arrayBuffer = await response.arrayBuffer();
+    // const buffer = Buffer.from(arrayBuffer);
+    const buffer = Buffer.from(response.data) 
+    console.log(buffer)
+    await fs.writeFileSync(path, buffer);
+}
 
-mediaStream.pipe(writeStream)
+const downloadLocalImage = async (url, path) => {
+    const encodedImage = fs.readFileSync(url)
+    // console.log(`Axios get status:${response.status}`)
+    // const blob = await response.blob();
+    // const arrayBuffer = await response.arrayBuffer();
+    // const buffer = Buffer.from(arrayBuffer);
+
+    console.log(encodedImage)
+    await fs.writeFileSync(path, encodedImage);
+}
+
+const localImage = '/home/mattq/Pictures/test.png'
+const sabePic = 'https://sabe.io/images/saturn.png'
+const twitPic = 'https://pbs.twimg.com/media/FiMjwwgVEAAMbif?format=jpg&name=medium'
+
+downloadImage(sabePic, "./sample-pic.png"); 
+downloadLocalImage(localImage, 'local-test.png')
