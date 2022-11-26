b679646c2fa30579a1d91bf38df3da3ba43fc654
neato0079 <neato0079@gmail.com>
2022-11-26 03:13:02 -0800

    Use node-fetch@2 to successfully download image from url

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 const axios = require('axios')
+const fetch = require('node-fetch')
 
 const downloadImage = async (url, path) => {
     const response = await axios.get(url);
-    console.log(`Axios get status:${response.status}`)
+    // console.log(response.data)
+    const base = await response.data.toString('base64')
+    // console.log(`BASE64: ${test}`)
+    // console.log(`Axios get status:${response.status}`)
     // const blob = await response.blob();
     // const arrayBuffer = await response.arrayBuffer();
     // const buffer = Buffer.from(arrayBuffer);
-    const buffer = Buffer.from(response.data) 
+    const buffer = Buffer.from(base) 
     console.log(buffer)
-    await fs.writeFileSync(path, buffer);
+    fs.writeFileSync(path, buffer);
 }
 
 const downloadLocalImage = async (url, path) => {
@@ -19,13 +23,33 @@ const downloadLocalImage = async (url, path) => {
     // const arrayBuffer = await response.arrayBuffer();
     // const buffer = Buffer.from(arrayBuffer);
 
-    console.log(encodedImage)
+    // console.log(encodedImage)
     await fs.writeFileSync(path, encodedImage);
+}
+
+const dl = async (path, url) => {
+    const response = await fetch(url);
+    console.log(typeof response)
+    const buffer = await response.buffer();
+    fs.writeFile(path, buffer, () => 
+      console.log('finished downloading!'));
+}
+
+const axiosDL = async (path, url) => {
+    const response = await axios.get(url);
+    console.log(response.data)
+    const buffer = await response.data.buffer();
+    fs.writeFile(path, buffer, () => 
+      console.log('finished downloading!'));
 }
 
 const localImage = '/home/mattq/Pictures/test.png'
 const sabePic = 'https://sabe.io/images/saturn.png'
 const twitPic = 'https://pbs.twimg.com/media/FiMjwwgVEAAMbif?format=jpg&name=medium'
 
-downloadImage(sabePic, "./sample-pic.png"); 
-downloadLocalImage(localImage, 'local-test.png')
+// downloadImage(sabePic, "./sample-pic.png"); 
+// downloadLocalImage(localImage, 'local-test.png')
+
+dl('./test-fetch.png', twitPic)
+
+// axiosDL('./test-fetch', twitPic)
