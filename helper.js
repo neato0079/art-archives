a3170457a1e88b4b4a166b13fa20847f3469c79e
neato0079 <neato0079@gmail.com>
2022-11-29 18:14:44 -0800

    Reorganize funcitons from main code

---
+++ helper.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const axios = require('axios');
+
+const dlAxios = async (url) => {
+    axios({
+        method: 'get',
+        url: 'http://bit.ly/2mTM3nY',
+        responseType: 'stream'
+      })
+        .then(function (response) {
+          response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+        });
+}
+
+const dl = async (path, url) => {
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    fs.writeFile(path, buffer, () =>
+        console.log('finished downloading!'));
+}
+
+const dlArray = async (urlArray) => {
+    let imageCount = 1;
+
+    urlArray.forEach(async url => {
+        const response = await fetch(url);
+        const buffer = await response.buffer();
+        fs.writeFile(`image${imageCount}.png`, buffer, () => {
+            console.log(`finished downloading image ${imageCount}!`);
+            imageCount += 1;
+        })
+    })
+}
+
+
+const localImage = '/home/mattq/Pictures/test.png';
+const sabePic = 'https://sabe.io/images/saturn.png';
+const twitPic = 'https://pbs.twimg.com/media/FiMjwwgVEAAMbif?format=jpg&name=medium';
+const redditPic = 'https://i.redd.it/jd8kg2f04f1a1.jpg';
+
+const imageArray = [sabePic, twitPic, redditPic]
+
+dlArray(imageArray)
+
+// dl('./test-fetch.png', twitPic)
+
+
