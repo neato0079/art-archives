89e68105919dade820eb6007138f528c79299103
neato0079 <neato0079@gmail.com>
2022-12-03 05:52:15 -0800

    GetUserMedia2 now returns an array of the user's media urls, but not all. Pagination issue

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-// https://developer.twitter.com/en/docs/twitter-ads-api/creatives/guides/identifying-media
+// Some of the following code follows this article https://developer.twitter.com/en/docs/tutorials/explore-a-users-tweets
 
-// All image cards include an image response attribute that contains the Twitter image URL. (For image app download cards, the name is wide_app_image.)
+// Use this reference to configure url endpoints for fetching user data https://developer.twitter.com/en/docs/twitter-api/users/lookup/api-reference/get-users
 
-// For Tweets, the media URL location depends on both the type of media and the endpoint being used. For Tweets with a single image, the URL can be found in entities["media"][0]["media_url"]. This is true for both the Ads API and the Standard API. When Tweets contain multiple images, however, the URLs can only be found extended_entities["media"][i]["media_url"]. This is only available in the Standard API.
-
-// Fetching media
-
-// As stated above, image cards do not contain references to either media IDs or media keys. As a result, it's not possible to fetch their assets through the Media Library. This is also true for Account Media images.
-
-// Video cards require that the video asset be part of the Media Library (or the Videos resource before it) prior to creating it. As a result, these assets will always be retrievable in the Media Library. This is also true for Account Media PREROLL assets.
-
-// Finally, media in Tweets are always guaranteed to be in the Media Library.
-
-// The following table summarizes which assets are retrievable in the Media Library, taking into account whether the resource response includes an identifier to use in the lookup.
-
-// The following code follows this article https://developer.twitter.com/en/docs/tutorials/explore-a-users-tweets
+// https://developer.twitter.com/en/docs/twitter-api/tweets/timelines/api-reference/get-users-id-tweets
 
 const axios = require('axios').default;
 require('dotenv').config();
@@ -40,7 +28,7 @@ const getUserID = async (userName) => {
 };
 
 const getTwitterTimeline = async () => {
-    const userID = await getUserID(myTwitterProfile); 
+    const userID = await getUserID(myTwitterProfile);
     console.log(userID);
     axios({
         method: 'get',
@@ -56,7 +44,7 @@ const getTwitterTimeline = async () => {
 };
 
 const getUserMedia = async () => {
-    const userID = await getUserID(myTwitterProfile); 
+    const userID = await getUserID(myTwitterProfile);
     // console.log(userID);
     axios({
         method: 'get',
@@ -75,11 +63,16 @@ const getUserMedia = async () => {
 };
 
 const getUserMedia2 = async () => {
-    const userID = await getUserID(myTwitterProfile); 
-    console.log(userID);
+    const userID = await getUserID(myTwitterProfile);
+    const userTimelineEndpoint = `https://api.twitter.com/2/users/${userID}/tweets?max_results=100&expansions=attachments.media_keys&media.fields=url`
+
+// `https://api.twitter.com/2/users/${userID}/tweets?tweet.fields=created_at&exclude=retweets,replies&max_results=100&expansions=attachments.media_keys&media.fields=url`
+
+    const sampleEndpoint = 'https://api.twitter.com/2/users/2244994945/tweets?tweet.fields=created_at&max_results=100&start_time=2019-01-01T17:00:00Z&end_time=2020-12-12T01:00:00Z'
+    // console.log(userID);
     axios({
         method: 'get',
-        url: `https://api.twitter.com/2/users/${userID}/tweets?exclude=retweets,replies&max_results=100&expansions=attachments.media_keys&media.fields=url`,
+        url: userTimelineEndpoint,
         // url: `https://api.twitter.com/2/users/${userID}/tweets?max_results=100`,
         // url: 'https://api.twitter.com/1.1/users/show.json?screen_name=twitterdev',
         headers: {
@@ -87,10 +80,18 @@ const getUserMedia2 = async () => {
         }
     })
         .then(response => {
-            // console.log(response);
-            const tweetObject = response.data.data
+            console.log(response);
+            const mediaLibrary = []
+            const userMedia = response.data.includes.media
+            for (media of userMedia) {
+                const mediaURL = media.url
+                if (mediaURL) {
+                    mediaLibrary.push(mediaURL);
+                }
+            }
             // console.log(JSON.stringify(tweetObject))
-            console.log(tweetObject)
+            console.log(mediaLibrary)
+            console.log(`Found ${mediaLibrary.length} media url(s)`)
         })
         .catch(e => { throw e });
 };
