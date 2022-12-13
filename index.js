aab1b4c47eee982e3e3cd2560c44c9e98fe57796
neato0079 <neato0079@gmail.com>
2022-12-13 01:38:22 -0800

    Fix formatting. Add comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 require('dotenv').config();
 const fs = require('fs');
 const downloadDirectory = './archives';
-const helper = require('./helper')
+const helper = require('./helper');
 
 const cliArgument = process.argv[2];
 
@@ -23,9 +23,9 @@ const getUserID = async (userName) => {
 };
 
 const continueBuildingMediaLibrary = async (userID, untilID) => {
-    console.log('Checking for more user media...')
+    console.log('Checking for more user media...');
 
-    const noRepliesOrRetweetsTwitterTimeline = `https://api.twitter.com/2/users/${userID}/tweets?exclude=retweets,replies&max_results=100&expansions=attachments.media_keys&media.fields=url&until_id=${untilID}`
+    const noRepliesOrRetweetsTwitterTimeline = `https://api.twitter.com/2/users/${userID}/tweets?exclude=retweets,replies&max_results=100&expansions=attachments.media_keys&media.fields=url&until_id=${untilID}`;
 
     return axios({
         method: 'get',
@@ -41,32 +41,32 @@ const continueBuildingMediaLibrary = async (userID, untilID) => {
 
                 return {
                     additionalMedia: mediaLibrary,
-                    updatedOldestTweet: response.data.meta.oldest_id
+                    updatedOldestTweet: response.data.meta.oldest_id // this returns undefined to stop the while loop @ line 95
                 }
-            }
-            const userMedia = response.data.includes.media
+            };
+            const userMedia = response.data.includes.media;
             for (media of userMedia) {
                 const mediaURL = media.url
                 if (mediaURL) {
                     mediaLibrary.push(mediaURL);
-                }
-            }
-            console.log(`Found ${userMedia.length} more media url(s)`)
+                };
+            };
+            console.log(`Found ${userMedia.length} more media url(s)`);
             return {
                 additionalMedia: mediaLibrary,
                 updatedOldestTweet: response.data.meta.oldest_id
-            }
+            };
         })
         .catch(e => {
-            console.log(`buildMediaLibrary problem? Here's the error!: ${e}`)
-            throw e
+            console.log(`buildMediaLibrary problem? Here's the error!: ${e}`);
+            throw e;
         });
 };
 
 const getAllUserMedia = async (twitterProfile) => {
     const userID = await getUserID(twitterProfile);
 
-    const firstCall = `https://api.twitter.com/2/users/${userID}/tweets?exclude=retweets,replies&max_results=100&expansions=attachments.media_keys&media.fields=url`
+    const firstCall = `https://api.twitter.com/2/users/${userID}/tweets?exclude=retweets,replies&max_results=100&expansions=attachments.media_keys&media.fields=url`;
 
     return axios({
         method: 'get',
@@ -76,37 +76,36 @@ const getAllUserMedia = async (twitterProfile) => {
         }
     })
         .then(async response => {
-            const oldestTweetResult = response.data.meta.oldest_id
-            let mediaLibrary = []
-            const userMedia = response.data.includes.media // this may not include all of the the user's media due to twitter API's response cap
+            const oldestTweetResult = response.data.meta.oldest_id;
+            let mediaLibrary = [];
+            const userMedia = response.data.includes.media; // this may not include all of the the user's media due to twitter API's response cap
             for (media of userMedia) {
-                const mediaURL = media.url
+                const mediaURL = media.url;
                 if (mediaURL) {
                     mediaLibrary.push(mediaURL);
                 }
             }
 
-            console.log(`Found ${mediaLibrary.length} media url(s) on first pass`)
+            console.log(`Found ${mediaLibrary.length} media url(s) on first pass`);
 
             // Check if there are additional media and push those to the current media library
             // This is done by checking the oldest tweet in the response and using that as the next starting point
             let { additionalMedia, updatedOldestTweet } = await continueBuildingMediaLibrary(userID, oldestTweetResult);
-            mediaLibrary.push(...additionalMedia)
+            mediaLibrary.push(...additionalMedia);
             while (updatedOldestTweet) {
                 const result = await continueBuildingMediaLibrary(userID, updatedOldestTweet);
-                mediaLibrary.push(...result.additionalMedia)
-                updatedOldestTweet = result.updatedOldestTweet
-                console.log(`Current total: ${mediaLibrary.length} media url(s)`)
-
+                mediaLibrary.push(...result.additionalMedia);
+                updatedOldestTweet = result.updatedOldestTweet;
+                console.log(`Current total: ${mediaLibrary.length} media url(s)`);
             }
 
             // console.log(mediaLibrary)
-            console.log(`Found ${mediaLibrary.length} media url(s)`)
-            return mediaLibrary
+            console.log(`Found ${mediaLibrary.length} media url(s)`);
+            return mediaLibrary;
         })
         .catch(e => {
-            console.log(`Twitter API problem? Here's the error!: ${e}`)
-            throw e
+            console.log(`Twitter API problem? Here's the error!: ${e}`);
+            throw e;
         });
 };
 
@@ -117,11 +116,11 @@ const main = async (twitterProfile) => {
     }
 
     const mediaURLs = await getAllUserMedia(twitterProfile);
-    console.log(mediaURLs)
+    console.log(mediaURLs);
 
-    helper.dlAxiosArray(twitterProfile, mediaURLs)
+    helper.dlAxiosArray(twitterProfile, mediaURLs);
 }
 // console.log(mediaURLs)
 
-main(cliArgument)
+main(cliArgument);
 
